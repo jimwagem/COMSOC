bb20566b1d0b984b4ee3cd35eb1b80a1fb5d8327
GeertenRijsdijk <geertenrijsdijk@hotmail.com>
2021-05-04 13:22:03 +0200

    small style update for dataloader

--- dataloader.py
+++ dataloader.py
@@ -15,48 +15,51 @@ class Project():
         self.selected = bool(words[6])
 
     def __str__(self):
-        return self.id
+        return str(self.id) + ': ' + self.name
 
 
 class RealDataLoader(data.Dataset):
     def __init__(self, filename, dropout = 0):
         self.filename = filename
-        self.mode = 0 # META / PROJECTS / VOTES
         self.dropout = dropout
-        self.projects = []
-        self.project_ids = []
+        self.load_file()
+
+    # Create x / target tensors from pabulib file
+    def load_file(self):
+        mode = 0 # META / PROJECTS / VOTES
         self.x = []
         self.targets = []
+        self.projects = []
+        self.project_ids = []
         with open(self.filename, 'r') as f:
             counter = 0
             for line in f:
                 line = line.strip('\n')
                 if line == 'PROJECTS':
-                    self.mode = 1
+                    mode = 1
                     counter = 0
                 elif line == 'VOTES':
-                    self.mode = 2
+                    mode = 2
                     counter = 0
                 elif counter == 1:
                     pass
-                elif self.mode == 1:
-                        p = Project(line)
-                        self.projects.append(p)
-                        self.project_ids.append(p.id)
-                elif self.mode == 2:
+                elif mode == 1:
+                    p = Project(line)
+                    self.projects.append(p)
+                    self.project_ids.append(p.id)
+                elif mode == 2:
                     t = -torch.ones(len(self.projects))
                     for vote in line.split(';')[4].split(','):
                         t[self.project_ids.index(int(vote))] = 1
                     self.targets.append(t)
 
-                    # Dropout some percent
+                    # Dropout some percent of x tensors
                     mask = torch.rand(len(self.projects))
                     zeros = torch.zeros(len(self.projects))
                     ones = torch.ones(len(self.projects))
                     mask = torch.where(mask < self.dropout, zeros, ones)
 
                     self.x.append(t * mask)
-
 
                 counter += 1
         self.x = torch.stack(self.x)
